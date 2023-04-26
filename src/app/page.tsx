302b96c9a76f9906acf88f80123405fcf94ce6ae
Mehmet <sumer682000@gmail.com>
2023-04-26 23:00:18 +0300

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,12 +28,12 @@ export default function Home() {
           </a>
         </div>
       </div>
-      <div className="flex items-center flex-col text-center mb-4">
+      <div className="flex items-center flex-col text-center mb-6 mt-12">
         <p>BM402 Dersi</p>
         <p>Vize Araştırma Ödevi</p>
         <p>Mehmet Şahin Sümer</p>
         <p>191180755</p>
-        <p className="mt-2">
+        <p className="mt-2 text-sm">
           Source code is available on this{" "}
           <a
             href="https://github.com/memetsumer/bm402-vize-191180755"
@@ -42,7 +42,7 @@ export default function Home() {
             GitHub Repo
           </a>
         </p>
-        <p className="text-white/80 mb-10">This page needs your location to retrieve data.</p>
+        <p className="text-white/80 mb-10 text-sm">This page needs your location to retrieve data.</p>
       </div>
 
       {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]"></div> */}
